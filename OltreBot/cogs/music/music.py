a6846a29a5e6d54ab8efca6c89ea2222bf34200b
HorusElohim <74312168+HorusElohim@users.noreply.github.com>
2022-01-20 20:37:52 +0100

    Update music.py

--- OltreBot/cogs/music/music.py
+++ OltreBot/cogs/music/music.py
@@ -377,7 +377,5 @@ class Music(commands.Cog):
         # Stop the current track so Lavalink consumes less resources.
         await player.stop()
         # Disconnect from the voice channel.
-        if ctx.author.voice.channel.id in self.embed_id:
-            del self.embed_id[ctx.author.voice.channel.id]
         await ctx.voice_client.disconnect(force=True)
         await self.send_msg(ctx, '*⃣ | Disconnected.')
